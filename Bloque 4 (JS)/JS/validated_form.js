f00b83c2dbd60eee02f97a91454f609753fe2cc9
Agustin Arquipa <juanarquipa6675@gmail.com>
2023-05-12 12:14:06 -0300

    Validacion terminada

--- Bloque 4 (JS)/JS/validated_form.js
+++ Bloque 4 (JS)/JS/validated_form.js
@@ -1,18 +1,75 @@
 //Para validar primero creamos una variable que apunte al formulario
-let form = document.getElementById('formulario');
+const form = document.getElementById('formulario');
 
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    validar();
+});
 
 function validar(){
+
     //la funcion trim, trae el string del input limpia
     let nameValue = document.getElementById('fullname');
+    let email = document.getElementById('email');
+    let phone = document.getElementById('phone');
+    let affair = document.getElementById('affair');
+    let mess = document.getElementById('message');
     //alert(nameValue);
+    //inicializamos una cadena para devolver un mensage
+    let warinigs = ""; //El warining estaba pensado para insertar en un small un mensaje de error
+    //Iniciamos una variable que controle que este correctamente ingresado el email
+    let regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ //Esto que insetarmos se conoce como patrones de evaluar
+    //Si en corchetes pone 2, 4 evualuamos aquellos correos con terminacion .info, etc
     //Validando el nombre
-    if(nameValue.value == '' ){
-        alert('Nombre vacio');
+    if(nameValue.value.length == '' ){
+        warinigs += "No podes dejar el nombre vacio!";
+        //alert(warinigs);
+        setErrorFor(nameValue, warinigs);
+    }else {
+        setSuccesFor(nameValue);
+    }
+    //Validamos el email
+    if(!regexEmail.test(email.value)) {
+        //Limpiamos el msj
+        warinigs = '';
+        warinigs += "Email invalidado!"
+        setErrorFor(email, warinigs);
+    }else {
+        setSuccesFor(email);
+    }
+    //Validamos el telefono
+
+    //Validamos el asunto
+    if(affair.value == ''){
+        warinigs = ''
+        warinigs += 'El asunto no puede esta vacio!'
+        setErrorFor(affair, warinigs);
+    }else {
+        setSuccesFor(affair)
+    }
+    //Validamos el msj
+    if(mess.value == ''){
+        warinigs = '';
+        warinigs += 'Mensaje Vacio!'
+        setErrorFor(mess, warinigs);
+    }else {
+        setSuccesFor(mess);
     }
 };
 
-form.addEventListener('submit', validar)
-
 //Aca hacemos la funcion para abrir el menu
 const toggleMenu = () => document.body.classList.toggle("open");
+
+//Aca hacemo la verificacion otra funcion
+function setErrorFor(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'contact-form error';
+    small.innerText = message;
+}
+
+function setSuccesFor(input){
+    const formControl = input.parentElement;
+    formControl.className = 'contact-form succes'
+}
